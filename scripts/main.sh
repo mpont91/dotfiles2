2d64f3ea77ab08a3e8122807d799db67eb9d6bcd
Marc Pont <slayerbleast@gmail.com>
2022-01-11 15:51:37 +0100

    Fix path scripts

--- scripts/main.sh
+++ scripts/main.sh
@@ -1,13 +1,16 @@
 #!/usr/bin/env bash
 
+set -euo pipefail
+export DOTFILES_PATH="$HOME/.dotfiles"
+
 sudo apt -qq update
 sudo apt -qq upgrade -y
 
-source drivers.sh
-source packages.sh
-source timezone.sh
-source aliases.sh
-source git.sh
-source apache2.sh
-source docker.sh
-source ssh-keys.sh
+source "$DOTFILES_PATH/scripts/drivers.sh"
+source "$DOTFILES_PATH/scripts/packages.sh"
+source "$DOTFILES_PATH/scripts/timezone.sh"
+source "$DOTFILES_PATH/scripts/aliases.sh"
+source "$DOTFILES_PATH/scripts/git.sh"
+source "$DOTFILES_PATH/scripts/apache2.sh"
+source "$DOTFILES_PATH/scripts/docker.sh"
+source "$DOTFILES_PATH/scripts/ssh-keys.sh"
