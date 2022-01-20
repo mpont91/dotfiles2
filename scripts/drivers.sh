d72eb8e0a874429dc431bf75374d46a92a56dd81
Marc Pont <slayerbleast@gmail.com>
2022-01-20 08:33:34 +0100

    check ubuntu-drivers

--- scripts/drivers.sh
+++ scripts/drivers.sh
@@ -2,4 +2,9 @@
 
 echo "🛠️ Setting up your drivers..."
 
-sudo ubuntu-drivers autoinstall
+if ! command -v ubuntu-drivers &> /dev/null
+then
+	echo "ubuntu-drivers could not be found"
+else
+	sudo ubuntu-drivers autoinstall
+fi
